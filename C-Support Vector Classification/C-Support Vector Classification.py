1b3fbf9d73df11b53b3f33e54847f8873b3d69a2
rr02048 <50341886+rr02048@users.noreply.github.com>
2019-08-21 16:06:40 -0400

    Update C-Support Vector Classification.py

--- C-Support Vector Classification/C-Support Vector Classification.py
+++ C-Support Vector Classification/C-Support Vector Classification.py
@@ -15,14 +15,30 @@ predictors = df.loc[:,'A1':'A15']
 
 
 #Fitting the model
-clf = SVC(gamma='auto', C= 0.0001)
+clf = SVC(gamma='auto', C= 0.0001, kernel = 'linear')
 Y = np.array(clssfr)
 Y = np.reshape(Y, (654,))
+y = Y
 X = np.array(predictors)
 clf.fit(X, Y) 
+
 
 # Perform classification on samples in X.
 prediction = clf.predict(X)
 
 # Returns the mean accuracy on the given test data and labels.
 training_error = clf.fit(X, Y).score(X, Y)
+
+params = clf.fit(X,Y).get_params()
+
+# Weights assigned to the features
+print('w = ',clf.coef_)
+
+#Constant in the decision function
+print('b = ',clf.intercept_)
+
+###
+print('Indices of support vectors = ', clf.support_)
+print('Support vectors = ', clf.support_vectors_)
+print('Number of support vectors for each class = ', clf.n_support_)
+print('Coefficients of the support vector in the decision function = ', np.abs(clf.dual_coef_))
